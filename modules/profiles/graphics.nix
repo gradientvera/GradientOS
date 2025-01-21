2661f11551609ade91d236aa692082784d3821f2
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-01-22 00:32:27 +0100

    Kill processes that cause the GPU to crash.

--- modules/profiles/graphics.nix
+++ modules/profiles/graphics.nix
@@ -22,6 +22,11 @@ in
       # Enable touchpad support
       services.libinput.enable = true;
 
+      # Taken from Bazzite at https://github.com/ublue-os/bazzite/blob/17c869dc70eede3f7066a8ad9ed07f46798fa9b3/system_files/deck/shared/usr/lib/udev/rules.d/80-gpu-reset.rules
+      services.udev.extraRules = ''
+        ACTION=="change", ENV{DEVNAME}=="/dev/dri/card0", ENV{RESET}=="1", ENV{PID}!="0", RUN+="${pkgs.coreutils}/bin/kill -9 %E{PID}"
+      '';
+
       environment.systemPackages = [
         pkgs.force-xwayland
       ];
