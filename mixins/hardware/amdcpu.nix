4a428941658afd9fb9278a07556d71e90220ac9d
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-01-22 00:13:23 +0100

    Enable SMU for AMD CPUs.

--- mixins/hardware/amdcpu.nix
+++ mixins/hardware/amdcpu.nix
@@ -8,6 +8,10 @@
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  # System Management Unit
+  hardware.cpu.amd.ryzen-smu.enable = true;
+  programs.ryzen-monitor-ng.enable = true;
+
   environment.systemPackages = with pkgs; [
     ryzenadj
     zenstates
