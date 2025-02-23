f118477fd03d791d83584c550d5bccd8b0455741
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-02-23 14:31:39 +0100

    Add extra commands to pufferpanel, wrap in FHS environment.

--- hosts/asiyah/pufferpanel.nix
+++ hosts/asiyah/pufferpanel.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   ports = import ./misc/service-ports.nix;
 in
@@ -6,8 +6,8 @@ in
 
   services.pufferpanel = {
     enable = true;
-    extraPackages = [ 
-      pkgs.gnutar
+    extraGroups = [
+      "podman"
     ];
     environment = {
       PUFFER_WEB_HOST = ":${toString ports.pufferpanel}";
@@ -16,6 +16,12 @@ in
       PUFFER_PANEL_REGISTRATIONENABLED = "false";
       PUFFER_PANEL_DATABASE_DIALECT = "postgresql";
       PUFFER_PANEL_DATABASE_URL = "host=localhost user=pufferpanel dbname=pufferpanel port=${toString ports.postgresql} sslmode=disable";
+    };
+    extraPackages = with pkgs; [ bash curl gawk gnutar gzip unzip ];
+    package = pkgs.buildFHSEnv {
+      name = "pufferpanel-fhs";
+      runScript = lib.getExe pkgs.pufferpanel;
+      targetPkgs = pkgs': with pkgs'; [ icu openssl zlib ];
     };
   };
 
