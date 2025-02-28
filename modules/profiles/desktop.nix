7c23c23c92540ae6d6104863e9d0451196c84ed6
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-02-28 22:00:59 +0100

    Set up SDDM compositor to kwin

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -153,6 +153,7 @@ in
       # Enable the KDE Plasma Desktop Environment.
       services.displayManager.sddm.enable = true;
       services.displayManager.sddm.wayland.enable = true;
+      services.displayManager.sddm.wayland.compositor = "kwin";
       services.desktopManager.plasma6.enable = true;
 
       programs.kdeconnect.enable = true;
