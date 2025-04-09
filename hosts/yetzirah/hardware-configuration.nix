e8ab6b6f302d82ce79074e2b9fa5537be4bf9fec
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-04-09 17:52:17 +0200

    Tweak wifi on yetzirah - Add patches for future kernel version support - Add parameter to switch to USB3.0 mode... doubles download speeds(!)

--- hosts/yetzirah/hardware-configuration.nix
+++ hosts/yetzirah/hardware-configuration.nix
@@ -8,8 +8,30 @@
   boot.kernelPackages = pkgs.linuxPackages_xanmod;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" "rtl8852au" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ rtl8852au ];
+  boot.kernelModules = [ "kvm-intel" "8852au" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    (rtl8852au.overrideAttrs (prevAttrs: {
+      patches = [
+        # Linux 6.13 support
+        (pkgs.fetchpatch {
+          name = "remove-MODULE_IMPORT-and-net_device-for-kernel-versions-over-6.13.patch";
+          url = "https://github.com/natimerry/rtl8852au/commit/c65ed43f42656aecf43e7ea80c58d204c3c67aca.patch";
+          hash = "sha256-l6ZBeI2PGQq11UBMiV/l8Ofry9FZH1KSBDwROXxnYHU=";
+        })
+        # Linux 6.14 support
+        (pkgs.fetchpatch {
+          name = "get_tx_power-callback-by-adding-link_id-parameter.patch";
+          url = "https://github.com/natimerry/rtl8852au/commit/91d168fc5aa818b4e85aa5b2b43d7f25470e925c.patch";
+          hash = "sha256-2dET83eVIU1PXdJJO1HWg3a81/6ztVwh0uN6VzXbG3o=";
+        }) 
+      ];
+    }))
+  ];
+
+  boot.kernelParams = [
+    # Switch to USB3.0 mode
+    "8852au.rtw_switch_usb_mode=1"
+  ];
 
   # Piece of shit fucking wifi adapter *fuck you*
   hardware.usb-modeswitch.enable = true;
