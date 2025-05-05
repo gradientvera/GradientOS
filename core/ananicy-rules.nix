8103a66c989b93dea18ce90e894db271453d1d5b
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-05-05 20:03:42 +0200

    Add WiVRn and ALVR to ananicy rules.

--- core/ananicy-rules.nix
+++ core/ananicy-rules.nix
@@ -4,4 +4,7 @@
   { name = "ustreamer"; type = "Player-Video"; }
   { name = "ffmpeg"; type = "Heavy_CPU"; }
   { name = "tdarr-ffmpeg"; type = "Heavy_CPU"; }
+  { name = "wivrn-server"; type = "LowLatency_RT"; }
+  { name = "wivrn-dashboard"; type = "LowLatency_RT"; }
+  { name = "alvr-dashboard"; type = "LowLatency_RT"; }
 ]
