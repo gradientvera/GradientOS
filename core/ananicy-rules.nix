49d72dd731ab74c7bb1c9a874ffae08cf9e615ce
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-05-09 17:55:55 +0200

    Add some steamvr executables to ananicy rules.

--- core/ananicy-rules.nix
+++ core/ananicy-rules.nix
@@ -5,7 +5,13 @@
   { name = "jellyfin"; type = "Player-Video"; }
   { name = "ffmpeg"; type = "Heavy_CPU"; }
   { name = "tdarr-ffmpeg"; type = "Heavy_CPU"; }
+
+  # Set VR relevant programs to low-latency
   { name = "wivrn-server"; type = "LowLatency_RT"; }
   { name = "wivrn-dashboard"; type = "LowLatency_RT"; }
   { name = "alvr-dashboard"; type = "LowLatency_RT"; }
+  { name = "vrserver"; type = "LowLatency_RT"; }
+  { name = "vrstartup"; type = "LowLatency_RT"; }
+  { name = "vrmonitor"; type = "LowLatency_RT"; }
+  { name = "vrcompositor"; type = "LowLatency_RT"; }
 ]
