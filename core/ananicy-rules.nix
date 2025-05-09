0f25797fac0684d9bb687a180cb1530ad05ef3d0
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-05-09 17:44:06 +0200

    Add jellyfin to ananicy rules.

--- core/ananicy-rules.nix
+++ core/ananicy-rules.nix
@@ -2,6 +2,7 @@
   # Using cachyos types, see: https://github.com/CachyOS/ananicy-rules/blob/master/00-types.types
   { name = "colmena"; type = "BG_CPUIO"; }
   { name = "ustreamer"; type = "Player-Video"; }
+  { name = "jellyfin"; type = "Player-Video"; }
   { name = "ffmpeg"; type = "Heavy_CPU"; }
   { name = "tdarr-ffmpeg"; type = "Heavy_CPU"; }
   { name = "wivrn-server"; type = "LowLatency_RT"; }
