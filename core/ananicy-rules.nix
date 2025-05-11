8876b4130e5ebaeea1432ce95edf7596a75b4d17
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-05-11 15:16:20 +0200

    Add ErsatzTV to ananicy rules.

--- core/ananicy-rules.nix
+++ core/ananicy-rules.nix
@@ -3,6 +3,7 @@
   { name = "colmena"; type = "BG_CPUIO"; }
   { name = "ustreamer"; type = "Player-Video"; }
   { name = "jellyfin"; type = "Player-Video"; }
+  { name = "ErsatzTV"; type = "Player-Video"; }
   { name = "ffmpeg"; type = "Heavy_CPU"; }
   { name = "tdarr-ffmpeg"; type = "Heavy_CPU"; }
 
