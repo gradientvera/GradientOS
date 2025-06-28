0246a77e2958bd00145a30ecf21b4e84e837f45d
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-06-28 12:24:31 +0200

    Fix featherine config

--- hosts/featherine/filesystems.nix
+++ hosts/featherine/filesystems.nix
@@ -8,14 +8,20 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-4b159de8-a815-46ef-94bd-52a9d0e03e3a".device = "/dev/disk/by-uuid/4b159de8-a815-46ef-94bd-52a9d0e03e3a";
-  boot.initrd.luks.devices."luks-4b159de8-a815-46ef-94bd-52a9d0e03e3a".bypassWorkqueues = true;
-  boot.initrd.luks.devices."luks-5300f6ce-cc89-429c-8656-50e5bf71f13d".device = "/dev/disk/by-uuid/5300f6ce-cc89-429c-8656-50e5bf71f13d";
-  boot.initrd.luks.devices."luks-5300f6ce-cc89-429c-8656-50e5bf71f13d".bypassWorkqueues = true;
+  boot.initrd.luks.devices."luks-4b159de8-a815-46ef-94bd-52a9d0e03e3a" = {
+    device = "/dev/disk/by-uuid/4b159de8-a815-46ef-94bd-52a9d0e03e3a";
+    bypassWorkqueues = true;
+  };
+  boot.initrd.luks.devices."luks-5300f6ce-cc89-429c-8656-50e5bf71f13d" = {
+    device = "/dev/disk/by-uuid/5300f6ce-cc89-429c-8656-50e5bf71f13d";
+    bypassWorkqueues = true;
+  };
 
   # SD Card
-  boot.initrd.luks.devices."luks-${auroraUuid}".device = "/dev/disk/by-uuid/${auroraUuid}";
-  boot.initrd.luks.devices."luks-${auroraUuid}".bypassWorkqueues = true;
+  boot.initrd.luks.devices."luks-${auroraUuid}" = {
+    device = "/dev/disk/by-uuid/${auroraUuid}";
+    bypassWorkqueues = true;
+  };
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
