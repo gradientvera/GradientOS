44ca1ebe2f4b281204dbcf8e2fc680a9fb91e04c
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-07-24 10:56:53 +0200

    Use SupplementaryGroups for uptime-kuma instead of disabling dynamicuser forgor this was a thing

--- hosts/asiyah/uptime-kuma.nix
+++ hosts/asiyah/uptime-kuma.nix
@@ -10,26 +10,10 @@
     };
   };
 
-  users.users.uptime-kuma = {
-    isSystemUser = true;
-    home = "/var/lib/uptime-kuma";
-    createHome = true;
-    homeMode = "750";
-    group = config.users.groups.uptime-kuma.name;
-    extraGroups = [ "podman" ];
-  };
-
-  users.groups.uptime-kuma = {};
-
   systemd.services.uptime-kuma = {
     serviceConfig = {
       # Needed for Docker/Podman integration
-      DynamicUser = lib.mkForce false;
-      User = lib.mkForce config.users.users.uptime-kuma.name;
-      Group = lib.mkForce config.users.groups.uptime-kuma.name;
-      PrivateTmp = lib.mkForce true;
-      RemoveIPC = lib.mkForce true;
-      RestrictSUIDSGID = lib.mkForce true;
+      SupplementaryGroups = "podman";
     };
   };
 
