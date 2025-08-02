69b88502909d2ccec7fd84d55b261c03720dc0e4
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-08-02 11:04:49 +0200

    Add victorialogs metrics to victoriametrics

--- hosts/asiyah/victoriametrics.nix
+++ hosts/asiyah/victoriametrics.nix
@@ -4,6 +4,18 @@
   services.victoriametrics = {
     enable = true;
     listenAddress = ":${toString ports.victoriametrics}";
+    prometheusConfig = {
+      scrape_configs = [
+        {
+          job_name = "VictoriaLogs";
+          static_configs = [
+            {
+              targets = [ "http://127.0.0.1:${toString ports.victorialogs}/metrics" ];
+            }
+          ];
+        }
+      ];
+    };
   };
 
   networking.firewall.interfaces.gradientnet.allowedTCPPorts = [
