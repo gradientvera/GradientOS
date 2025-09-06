c69be926cfb24084ca1feb1eaa56214c2510916d
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-09-06 22:42:33 +0200

    Use QEMU from unstable

--- hosts/asiyah/libvirtd/default.nix
+++ hosts/asiyah/libvirtd/default.nix
@@ -10,7 +10,7 @@
     onBoot = "ignore";
     onShutdown = "suspend";
     qemu = {
-      package = pkgs.stable.qemu_full; # TODO: remove stable...
+      package = pkgs.qemu_full;
       runAsRoot = true;
       ovmf.enable = true;
       swtpm.enable = true;
