fffc540476ed449ac774dc97cc33c0fd879831b4
Vera Aguilera Puerto <gradientvera@outlook.com>
2025-12-29 13:02:13 +0100

    Properly override old ExecStart for only-suspend-then-hibernate

--- mixins/only-suspend-then-hibernate.nix
+++ mixins/only-suspend-then-hibernate.nix
@@ -2,6 +2,10 @@
 {
 
   # Always suspend-then-hibernate
-  systemd.services.systemd-suspend.serviceConfig.ExecStart = lib.mkForce "${pkgs.systemd}/lib/systemd/systemd-sleep suspend-then-hibernate";
+  systemd.services.systemd-suspend.serviceConfig.ExecStart = lib.mkForce [ 
+    # Remove existing ExecStart line
+    ""
+    "${pkgs.systemd}/lib/systemd/systemd-sleep suspend-then-hibernate"
+  ];
 
 }
