9f691ca387f45722202ba071fd3df3731add7d4e
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-01-17 11:24:30 +0100

    Tweaks to robot vacuum photo publish script, allow to take photo once

--- misc/vacuum/gradient_publish_photo.sh
+++ misc/vacuum/gradient_publish_photo.sh
@@ -11,9 +11,11 @@ exec 2>&1
 
 trap 'echo "Interrupted, exiting camera publish now..."; exit 1' INT
 
+export MODE=${1:-daemon}
+
 GetVars () {
     export VALETUDO_CONFIG="/data/valetudo_config.json"
-    export VALETUDO_ID=$(curl 'http://127.0.0.1:80/api/v2/valetudo' -H 'accept: application/json' | jq .systemId -r)
+    export VALETUDO_ID=$(curl -s 'http://127.0.0.1:80/api/v2/valetudo' -H 'accept: application/json' | jq .systemId -r)
     export VALETUDO_ID_LOWER=$(echo $VALETUDO_ID | tr '[:upper:]' '[:lower:]')
     export VALETUDO_FRIENDLY_NAME=$(cat $VALETUDO_CONFIG | jq .valetudo.customizations.friendlyName -r)
     export VALETUDO_VERSION=$(cat $VALETUDO_CONFIG | jq ._version -r)
@@ -41,23 +43,29 @@ EOF
 }
 
 GetVars
-mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -t "homeassistant/camera/${VALETUDO_ID}/${VALETUDO_ID}_gradient_camera/config" -r -m "${AUTODISCOVER}"
+mosquitto_pub --quiet -h "$MQTT_HOST" -p "$MQTT_PORT" -t "homeassistant/camera/${VALETUDO_ID}/${VALETUDO_ID}_gradient_camera/config" -r -m "${AUTODISCOVER}"
 
 while true; do
-    sleep 5
+    if [ "$MODE" != "once" ]; then
+        sleep 5
+    fi;
 
     # Get them every time in case some value changes
     GetVars
 
-    if [ "$VALETUDO_DOCKED" = "true" ]; then
+    if [ "$VALETUDO_DOCKED" = "true" ] && [ "$MODE" != "once" ]; then
         continue
     fi;
 
-    camerademo NV21 640 480 0 bmp /tmp 1 >> /dev/null || continue
+    camerademo YUV420 1632 1224 0 bmp /tmp 1 >> /dev/null || continue
 
-    magick /tmp/bmp_NV21_1.bmp /tmp/bmp_NV21_1.jpg || continue
+    magick /tmp/bmp_YUV420_1.bmp /tmp/bmp_YUV420_1.jpg >> /dev/null || continue
 
-    mosquitto_pub -h "$MQTT_HOST" -p "$MQTT_PORT" -t $TOPIC -r -f /tmp/bmp_NV21_1.jpg || continue
+    mosquitto_pub --quiet -h "$MQTT_HOST" -p "$MQTT_PORT" -t $TOPIC -r -f /tmp/bmp_YUV420_1.jpg || continue
 
-    rm /tmp/bmp_NV21_1.bmp /tmp/bmp_NV21_1.jpg || continue
+    rm /tmp/bmp_YUV420_1.bmp /tmp/bmp_YUV420_1.jpg >> /dev/null || continue
+
+    if [ "$MODE" == "once" ]; then
+        exit 0
+    fi
 done
