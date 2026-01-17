e79d14c5ff40e98ab4a81d2f2195183ad4f81c3e
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-01-17 11:22:10 +0100

    Use OpenVino for Frigate better than c*ral

--- hosts/asiyah/frigate.nix
+++ hosts/asiyah/frigate.nix
@@ -12,9 +12,8 @@ in
     hostname = "frigate.asiyah.gradient.moe";
     vaapiDriver = "iHD";
     settings = {
-      audio.enabled = true;
-      birdseye.enabled = true;
-      birdseye.restream = true;
+      birdseye.enabled = false;
+      birdseye.restream = false;
       birdseye.mode = "continuous";
       detect.enabled = true;
       motion.enabled = true;
@@ -29,10 +28,58 @@ in
       ffmpeg.path = pkgs.ffmpeg-full;
       ffmpeg.hwaccel_args = "preset-intel-qsv-h264";
 
-      /*detectors.coral = {
-        type = "edgetpu";
-        device = "pci";
-      };*/
+      detectors = {
+        ov_0 = {
+          type = "openvino";
+          device = "GPU";
+        };
+        ov_1 = {
+          type = "openvino";
+          device = "GPU";
+        };
+      };
+
+      # Stateful step: https://docs.frigate.video/configuration/object_detectors/#yolov9
+      model = {
+        model_type = "yolo-generic";
+        width = 320; # <--- should match whatever was set in notebook
+        height = 320; # <--- should match whatever was set in notebook
+        input_tensor = "nchw";
+        input_dtype = "float";
+        path = "/var/lib/frigate/yolov9-t-320.onnx";
+        labelmap_path = "/var/lib/frigate/coco-80.txt";
+      };
+
+      audio = {
+        enabled = true;
+        listen = [
+          "fire_alarm"
+          "explosion"
+          "shatter"
+          "scream"
+          "speech"
+          "glass"
+          "yell"
+          "meow"
+        ];
+      };
+
+      semantic_search = {
+        enabled = false; # borked 
+        model_size = "large";
+      };
+
+      face_recognition = {
+        enabled = false; # borked
+        model_size = "large";
+      };
+
+      genai = {
+        enabled = false; # borked
+        provider = "ollama";
+        base_url = "http://127.0.0.1:${toString ports.ollama}";
+        model = "llava:7b";
+      };
 
       # Extremely important! Makes webrtc and two-way audio work
       go2rtc = config.services.go2rtc.settings;
@@ -46,16 +93,16 @@ in
             "Main Stream" = "eufy-e220-hallway";
             "Sub Stream" = "eufy-e220-hallway_sub";
           };
-          ffmpeg.output_args.record = "preset-record-generic-audio-copy";
+          ffmpeg.output_args.record = "preset-record-generic-audio-aac";
           ffmpeg.inputs = [
             {
-              path = "rtsp://127.0.0.1:${toString ports.go2rtc-rtsp}/eufy-e220-hallway?timeout=30";
-              input_args = "preset-rtsp-restream-low-latency";
+              path = "rtsp://127.0.0.1:${toString ports.go2rtc-rtsp}/eufy-e220-hallway?timeout=60";
+              input_args = "preset-rtsp-restream";
               roles = [ "record" ];
             }
             {
-              path = "rtsp://127.0.0.1:${toString ports.go2rtc-rtsp}/eufy-e220-hallway_sub?timeout=30";
-              input_args = "preset-rtsp-restream-low-latency";
+              path = "rtsp://127.0.0.1:${toString ports.go2rtc-rtsp}/eufy-e220-hallway_sub?timeout=60";
+              input_args = "preset-rtsp-restream";
               roles = [ "detect" "audio" ];
             }
           ];
@@ -75,6 +122,12 @@ in
             main = {
               coordinates =	"0,0,1,0,1,1,0,1";
             };
+          };
+          genai = {
+            enabled = false; # borked
+            use_snapshots = true;
+            objects = [ "person" ];
+            required_zones = [ "main" ];
           };
         };
       };
@@ -100,10 +153,12 @@ in
       streams = {
         # See https://github.com/AlexxIT/go2rtc/issues/857
         eufy-e220-hallway = [
-          "rtsp://thingino:thingino@${localAddresses.eufy-e220-hallway-ip}/ch0#timeout=30"
+          "rtsp://thingino:thingino@${localAddresses.eufy-e220-hallway-ip}/ch0#timeout=60"
+          "ffmpeg:eufy-e220-hallway#audio=opus"
         ];
         eufy-e220-hallway_sub = [
-          "rtsp://thingino:thingino@${localAddresses.eufy-e220-hallway-ip}/ch0#timeout=30"
+          "rtsp://thingino:thingino@${localAddresses.eufy-e220-hallway-ip}/ch1#timeout=60"
+          "ffmpeg:eufy-e220-hallway_sub#audio=opus"
         ];
       };
     };
@@ -113,8 +168,9 @@ in
     enableACME = false;
     useACMEHost = "gradient.moe";
     quic = true;
-    forceSSL = true;
+    addSSL = true;
     extraConfig = ''
+      allow 127.0.0.1/8;
       allow ${config.gradient.const.wireguard.addresses.gradientnet.gradientnet}/24;
       deny all;
     '';
