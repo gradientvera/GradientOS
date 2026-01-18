273bf8cff85617b600ad9712b93a79ad1b702968
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-01-18 21:50:59 +0100

    Better udev rules to recover from graphics crashes sheesh, my GPU crashes quite often lately

--- modules/profiles/graphics.nix
+++ modules/profiles/graphics.nix
@@ -24,7 +24,8 @@ in
 
       # Taken from Bazzite at https://github.com/ublue-os/bazzite/blob/17c869dc70eede3f7066a8ad9ed07f46798fa9b3/system_files/deck/shared/usr/lib/udev/rules.d/80-gpu-reset.rules
       services.udev.extraRules = ''
-        ACTION=="change", ENV{DEVNAME}=="/dev/dri/card0", ENV{RESET}=="1", ENV{PID}!="0", RUN+="${pkgs.coreutils}/bin/kill -9 %E{PID}"
+        ACTION=="change", SUBSYSTEM=="drm", KERNEL=="card[0-9]*", ENV{RESET}=="1", ENV{PID}!="0", RUN+="${pkgs.coreutils}/bin/kill -9 %E{PID}"
+        ACTION=="change", SUBSYSTEM=="drm", KERNEL=="card[0-9]*", ENV{RESET}=="1", ENV{FLAGS}=="1", RUN+="${pkgs.systemd}/bin/systemd-run --no-block --no-ask-password --service-type=oneshot ${pkgs.systemd}/bin/systemctl restart display-manager.service"
       '';
 
       environment.systemPackages = [
