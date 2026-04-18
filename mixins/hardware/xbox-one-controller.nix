769a1ca8e542da0a9fe4917ce157306634ce9954
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-04-18 11:03:37 +0200

    Fix broken string

--- mixins/hardware/xbox-one-controller.nix
+++ mixins/hardware/xbox-one-controller.nix
@@ -4,6 +4,6 @@
 
   # Xbox one controller bluetooth workaround
   hardware.xpadneo.enable = true;
-  boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
+  boot.extraModprobeConfig = "options bluetooth disable_ertm=1";
 
 }
