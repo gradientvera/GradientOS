df4697299990cb75073b505b3bc130d21b24e4dd
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-04-23 18:17:59 +0200

    Set locale

--- mixins/neith-locale.nix
+++ mixins/neith-locale.nix
@@ -2,7 +2,7 @@
 {
 
   # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "Europe/Madrid";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
