692ed50fb846d9617352e357f06cf279e8e82184
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-04-23 18:17:45 +0200

    Disable USB autosuspend for input devices with udev rule

--- core/udev.nix
+++ core/udev.nix
@@ -21,6 +21,12 @@
     # HDD parameters
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", \
         ATTRS{id/bus}=="ata", RUN+="${pkgs.hdparm}/bin/hdparm -B 254 -S 0 /dev/%k"
+  
+    # -- Power --
+    # Disable autosuspend for input USB devices
+    ACTION=="add", SUBSYSTEM=="input", TEST=="power/control", ATTR{power/control}="on"
+    ACTION=="add", SUBSYSTEM=="input", TEST=="power/autosuspend", ATTR{power/autosuspend}="-1"
+    ACTION=="add", SUBSYSTEM=="input", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}="-1"
   '';
 
 }
