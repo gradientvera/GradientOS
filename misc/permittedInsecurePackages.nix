d78cb2bf285c707a67304c436f82370996831556
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-06-12 22:00:37 +0200

    Add bitwarden electron version to permitted insecure packages smh

--- misc/permittedInsecurePackages.nix
+++ misc/permittedInsecurePackages.nix
@@ -4,4 +4,5 @@
   "libsoup-2.74.3" # TODO: Remove when not needed anymore
   "python3.14-ecdsa-0.19.2"
   "openssl-1.1.1w" # TODO: Remove, thanks discord
+  "electron-39.8.10" # TODO: Remove, thanks bitwarden
 ]
