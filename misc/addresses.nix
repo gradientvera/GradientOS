3b4989b8b13790b11c0e97c255952285a0923df1
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-07-19 12:39:46 +0200

    Add tailscale-domain to addresses

--- misc/addresses.nix
+++ misc/addresses.nix
@@ -3,6 +3,7 @@
   briahv6 = "2a01:4f8:1c1a:5e97::1";
   briahv6-cidr = "2a01:4f8:1c1a:5e97::/64";
 
+  tailscale-domain = "tailnet.constellation.moe";
   tailscale-ipv4 = "100.64.0.0";
   tailscale-ipv6 = "fd7a:115c:a1e0::";
   tailscale-ipv4-cidr = "100.64.0.0/10";
