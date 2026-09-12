636e6789961a68604679ef41daf30d982fccc968
Vera Aguilera Puerto <gradientvera@outlook.com>
2026-09-12 21:54:37 +0200

    Add backup that's a bit more often Yep yep-

--- hosts/asiyah/backups.nix
+++ hosts/asiyah/backups.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ config, ... }:
+let
+  secrets = config.sops.secrets;
+in
 {
 
   services.restic.backups.hokma = {
@@ -10,6 +13,24 @@
       "/data/gradient-data"
       "/data/lfs"
       "/data/repositories"
+    ];
+  };
+
+  services.restic.backups.pi = {
+    initialize = true;
+    repositoryFile = secrets.hokma-repository.path;
+    passwordFile = secrets.hokma-password.path;
+    environmentFile = secrets.hokma-environment.path;
+    extraOptions = [
+      "sftp.args='-4 -o StrictHostKeyChecking=accept-new -i ${config.sops.secrets.backups-ssh-priv.path}'"
+    ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "1h";
+    };
+    paths = [
+      "/home/vera/.pi"
     ];
   };
 
